a6a32636679f54082fe5d20a01d40647c64e5ce3
anntisa <annietsai92@gmail.com>
2015-03-09 16:11:52 -0700

    made descriptions more angularjs dynamic

--- app/components/home/HomeController.js
+++ app/components/home/HomeController.js
@@ -1,4 +1,7 @@
 myAppControllers.controller('HomeController', ['$scope',
 	function($scope) {
-		$scope.pageTitle = "Annie's Home Page";
-}]);
+		$scope.pageTitle = "Welcome to my Home Page!";
+        $scope.WebDescription = "This is my playground for all things web. I will constantly be updating this site for improvements or new features. Stay tuned!";
+        $scope.AboutMe = "I am an avid Web Developer with a background in Computer Science at the Georgia Institute of Technology. My goal is to learn all things development. This site is my way of tapping into the web development/designing world. Hope you guys like it!";
+    }
+]);
